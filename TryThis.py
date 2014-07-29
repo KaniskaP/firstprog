ac99e4797ff770b37150342f3b6201ce8f0b03ab
kaniska2010 <kaniska2008@gmail.com>
2014-07-29 23:02:04 +0530

    build2

--- TryThis.py
+++ TryThis.py
@@ -3,10 +3,12 @@ __author__ = 'Kan!skA'
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
+# browser = webdriver.Firefox()
+browser = webdriver.Chrome('C:/Users/Kan!skA/PycharmProjects/chromedriver.exe')
+# browser = webdriver.Ie('C:/Users/Kan!skA/PycharmProjects/IEDriverServer.exe')
 
-browser.get('http://www.yahoo.com')
-assert 'Yahoo' in browser.title
-
-elem = browser.find_element_by_name('p')  # Find the search box
-elem.send_keys('seleniumhq' + Keys.RETURN)
+browser.get('http://www.google.com')
+assert 'Google' in browser.title
+href = browser.find_element_by_name('q')  # Find the search box
+href.send_keys('kaniska.in' + Keys.RETURN)
+browser.quit()
