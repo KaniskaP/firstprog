a68c0d4db7da45cc3118ba4d7a176569dd5ee88b
kaniska2010 <kaniska2008@gmail.com>
2014-07-30 00:57:21 +0530

    build2

--- TryThis.py
+++ TryThis.py
@@ -3,8 +3,8 @@ __author__ = 'Kan!skA'
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# browser = webdriver.Firefox()
-browser = webdriver.Chrome('C:/Users/Kan!skA/PycharmProjects/chromedriver.exe')
+browser = webdriver.Firefox()
+# browser = webdriver.Chrome('C:/Users/Kan!skA/PycharmProjects/chromedriver.exe')
 # browser = webdriver.Ie('C:/Users/Kan!skA/PycharmProjects/IEDriverServer.exe')
 
 browser.get('http://www.google.com')
---
+++ setup.py
@@ -0,0 +1,38 @@
+# =============================================================================
+# Author: Kan!skA
+#     Date:   Jul 30, 2014
+#     File:  setup.py
+#     Description: This is the cx_Freeze setup file for creating an exe program
+# =============================================================================
+from cx_Freeze import setup, Executable
+# NOTE: you can include any other necessary external imports here aswell
+
+includefiles = []  # include any files here that you wish
+includes = []
+excludes = []
+packages = []
+
+exe = Executable(
+    # what to build
+    script="TryThis.py",  # the name of your main python script goes here
+    initScript=None,
+    base="Win32GUI",  # if creating a GUI instead of a console app, type "Win32GUI"
+    targetName="First.exe",  # this is the name of the executable file
+    copyDependentFiles=True,
+    compress=True,
+    appendScriptToExe=True,
+    appendScriptToLibrary=True,
+    icon=None  # if you want to use an icon file, specify the file name here
+)
+
+setup(
+    # the actual setup & the definition of other misc. info
+    name="Try This",  # program name
+    version="0.1",
+    description='trial',
+    author="Kan!skA",
+    author_email="email@kaniska.in",
+    options={"build_exe": {"excludes": excludes, "packages": packages,
+                           "include_files": includefiles}},
+    executables=[exe], requires=['cx_Freeze']
+)
